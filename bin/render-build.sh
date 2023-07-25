50af74b9b70d5b1fab04bb222466f391f1400adf
Rohit Kushwaha <rohit.kushwaha@w3villa.com>
2023-07-25 12:51:57 +0530

    initail commit

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -8,5 +8,5 @@ bundle exec rake assets:clean
 
 # These are not combined because we want to reload all models after the migrations take place.
 # TODO Any way to read these from the `Procfile` where we have the same thing defined for Heroku?
-bundle exec rails db:migrate 
+bundle exec rails db:migrate
 bundle exec rails db:seed
